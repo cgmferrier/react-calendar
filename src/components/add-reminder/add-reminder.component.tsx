30b355148e171dc6f36d098ee91b9dc6ee4a6917
Cecile Ferrier <cgmferrier@gmail.com>
2020-07-05 18:59:22 +0100

    Created color picker component

--- src/components/add-reminder/add-reminder.component.tsx
+++ src/components/add-reminder/add-reminder.component.tsx
@@ -1,6 +1,7 @@
 import MomentUtils from '@date-io/moment';
 import { Button } from '@material-ui/core';
 import { KeyboardDatePicker, KeyboardTimePicker, MuiPickersUtilsProvider } from '@material-ui/pickers';
+import ColorPicker from 'components/color-picker/color-picker.component';
 import moment from 'moment';
 import React from 'react';
 import { connect, useSelector } from 'react-redux';
@@ -16,6 +17,11 @@ const DatePicker = () => {
     console.log('use store to remove modal nodes')
   }
 
+  const updateColor = (color: any) => {
+    // assign color to  new reminder
+    console.log(color);
+  }
+
   const updateDate = (date: any) => {
     setSelectedDate(date);
   };
@@ -23,53 +29,48 @@ const DatePicker = () => {
   return (
     <div className='modal__wrapper'>
       <div className='add-reminder'>
-        <div className='form-element'>
-          <label htmlFor='reminder-name'>Name for reminder</label>
-          <input id='reminder-name' type='text'/>
-        </div>
-        <div className='add-reminder__pickers'>
-          <MuiPickersUtilsProvider utils={MomentUtils}>
-            <KeyboardDatePicker
-              className='add-reminder__picker'
-              disableToolbar
-              format='DD/MM/yyyy'
-              id='reminder-date-picker'
-              label='Choose a date'
-              margin='normal'
-              onChange={updateDate}
-              value={selectedDate}
-              variant='inline'
-            />
-            <KeyboardTimePicker
-              ampm={false}
-              className='add-reminder__picker'
-              margin="normal"
-              id="time-picker"
-              label="Time picker"
-              value={selectedDate}
-              onChange={updateDate}
-              variant='inline'
-            />
-          </MuiPickersUtilsProvider>
-          <div className='form-element form-element--no-padding'>
-            <label htmlFor='reminder-color-picker'>Choose a color</label>
-            <select id='reminder-color-picker'>
-              <option>Red</option>
-              <option>White</option>
-              <option>Blue</option>
-            </select>
+        <form className='form'>
+          <div className='form-element'>
+            <label htmlFor='reminder-name'>Name for reminder</label>
+            <input id='reminder-name' type='text'/>
           </div>
-        </div>
-        <div className='form-element'>
-          <label htmlFor='reminder-description'>Description</label>
-          <textarea id='reminder-description'/>
-        </div>
-        <div className='form__actions'>
-          <Button variant="outlined" color='primary' onClick={closeModal}>Cancel</Button>
-          <Button variant="contained" color="primary">
-            Create reminder
-          </Button>
-        </div>
+          <div className='add-reminder__pickers'>
+            <MuiPickersUtilsProvider utils={MomentUtils}>
+              <KeyboardDatePicker
+                className='add-reminder__picker'
+                disableToolbar
+                format='DD/MM/yyyy'
+                id='reminder-date-picker'
+                label='Choose a date'
+                margin='normal'
+                onChange={updateDate}
+                value={selectedDate}
+                variant='inline'
+              />
+              <KeyboardTimePicker
+                ampm={false}
+                className='add-reminder__picker'
+                margin="normal"
+                id="time-picker"
+                label="Time picker"
+                value={selectedDate}
+                onChange={updateDate}
+                variant='inline'
+              />
+            </MuiPickersUtilsProvider>
+          </div>
+          <ColorPicker updateColor={updateColor}/>
+          <div className='form-element'>
+            <label htmlFor='reminder-description'>Description</label>
+            <textarea id='reminder-description'/>
+          </div>
+          <div className='form__actions'>
+            <Button variant="outlined" color='primary' onClick={closeModal}>Cancel</Button>
+            <Button variant="contained" color="primary">
+              Create reminder
+            </Button>
+          </div>
+        </form>
       </div>
     </div>
   )
